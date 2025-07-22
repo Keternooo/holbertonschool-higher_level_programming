8be1ea22b1b0da450a1b3d0337e7ad84711ba017
root <keterno@proton.me>
2025-07-22 09:26:43 +0000

    Second day

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3  
-
-
-
-for alpha_letters in range(ord('a'), ord('z')+1):
-    if alpha_letters == 'e' or alpha_letters == 'q':
-       continue
-    print("{:c}".format(alpha_letters), end="")
+for i in range(ord('a'), ord('z')+1):
+    letter = chr(i)
+    if letter not in "qe":
+        print(letter, end="")
