9950091615e1d950fc0febf705a4fdc4cd8d08d2
root <keterno@proton.me>
2025-07-22 09:28:07 +0000

    Second day

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3  
-for i in range(ord('a'), ord('z')+1):
-    letter = chr(i)
-    if letter not in "qe":
-        print(letter, end="")
+for alpha_letters in range(ord('a'), ord('z')+1):
+    if alpha_letters in [101,113]:
+    continue
+    print("{:c}".format(alpha_letters), end="")
