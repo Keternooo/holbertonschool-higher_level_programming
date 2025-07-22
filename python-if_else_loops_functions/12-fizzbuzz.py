e69b9703e366645125cd7f2d294e7aa9521b2296
root <keterno@proton.me>
2025-07-22 14:12:03 +0000

    Second day

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        str = 'Fizz' if i % 3 == 0 else ''
+        str += 'Buzz' if i % 5 == 0 else ''
+        str = i if str == '' else str
+        print(str, end='')
