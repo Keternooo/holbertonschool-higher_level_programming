87ec8fdfcb994d03e5c84b0b2b7cb5e27fef95de
root <keterno@proton.me>
2025-07-22 13:17:11 +0000

    Second day

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
     newString = ''
-    for i in range(str):
-        charconvert = ord(str[i])
-        if 97 <= charconvert <= 122:
-            newString += chr(ord(charconvert) - 32)
+    for i in range(len(str)):
+        if str[i].islower():
+            charconvert = ord(str[i])
+            newString += chr(charconvert - 32)
         else:
             newString += str[i]
+    print(newString)
+
+uppercase('Cououucou jdhdhj 665')
