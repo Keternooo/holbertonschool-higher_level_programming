d4c55635adf904a96c11c0d9405107f4922d4052
root <keterno@proton.me>
2025-07-23 07:48:05 +0000

    .

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""program that creates the class Square defined by its size"""
+
+
+class Square:
+    """size is a private attribute of the class Square which is an integer"""
+
+    def __init__(self, size = 0):
+        """verify value for size and default def by 0"""
+        if type(size) is not int: 
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError('size must be >= 0')
+        self.__size = size
