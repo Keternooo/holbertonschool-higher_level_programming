ef8443562d32ea98c3923f2bbd6a19e6103c4685
root <keterno@proton.me>
2025-07-23 13:28:38 +0000

    Third Day

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_empty(self):
+        self.assertTrue(max_integer([1,2,3,4]), 4)
