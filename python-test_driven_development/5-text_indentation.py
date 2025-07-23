b2ac4dfdaffcf13e33b885879bf92002630135a1
root <keterno@proton.me>
2025-07-23 13:03:33 +0000

    Third Day

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+""" A simple module for indentation """
+
+
+def text_indentation(text):
+    """ method for indentation """
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+
+    arrSymbols = [':', '.', '?']
+    for i in text:
+        print(i, end="")
+        if i in arrSymbols:
+            print('')
