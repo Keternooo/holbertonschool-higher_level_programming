068cf702441db9d255879c429cc71648f5f36d10
root <keterno@proton.me>
2025-07-23 09:58:50 +0000

    Third Day

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
-#!/bin/usr/python3
-
+#!/usr/bin/python3
+"""Simple module for add two intergers"""
 def add_integer(a, b=98):
+    """My method for add two int"""
     if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
     elif isinstance(b, (int, float)):
