2ba85d0dd7a8a7098296bcefee66d8526599cb1f
root <keterno@proton.me>
2025-07-23 13:33:33 +0000

    Third Day

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,6 +8,3 @@ class TestMaxInteger(unittest.TestCase):
 
     def max_at_the_end(self):
         self.assertEqual(max_integer([1,2,3,4]), 4)
-
-    def max_at_middle(self):
-        self.assertEqual(max_interger([1,5,2]), 5)
