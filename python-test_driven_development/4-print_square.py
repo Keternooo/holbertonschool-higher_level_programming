eee904931966af1e8c19af1d6f98fad208c5280f
root <keterno@proton.me>
2025-07-23 12:42:14 +0000

    Third Day

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,7 +5,7 @@
 
 def print_square(size):
     """ Method for print square """
-    if size is not int:
+    if type(size) is not int:
         raise TypeError('size must be an integer')
     elif size < 0:
         raise ValueError('size must be >= 0')
