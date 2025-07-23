0b58ec32b94c034cdb8cc20aaf47998cabf85b69
root <keterno@proton.me>
2025-07-23 09:37:58 +0000

    Third Day

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+
+def add_integer(a, b=98):
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+    elif isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+    else:
+        return a + b
