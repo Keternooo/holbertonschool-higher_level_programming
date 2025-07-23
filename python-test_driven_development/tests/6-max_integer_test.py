5987d8be7d61edefb6e601a8c68bb79c3253e3b9
root <keterno@proton.me>
2025-07-23 13:38:55 +0000

    Third Day

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -14,3 +14,4 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([15, 10, 5]), 15)
         self.assertEqual(max_integer([15, 10, 5, -5, -10, 15]), 15)
         self.assertEqual(max_integer([15, 15, 15]), 15)
+        self.assertEqual(max_integer([10, 15, 10]), 15)
