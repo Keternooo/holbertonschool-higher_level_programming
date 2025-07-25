f0986c15f37ac2eb450600d60526dfd666ad1cca
root <keterno@proton.me>
2025-07-25 12:47:17 +0000

    Firth Day

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], (e, s, b) => {
-	if(e) return;
-	console.log([...(b.matchAll('/people/18/'))].length);
+  if(e) return;
+  console.log([...(b.matchAll('/people/18/'))].length);
 });
