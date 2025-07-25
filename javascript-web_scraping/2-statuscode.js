b84c9d9ecf9e7ccb928622ef88949becf1c1bbee
root <keterno@proton.me>
2025-07-25 11:56:55 +0000

    Firth Day

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
-  console.log(response && response.statusCode); // Print the response status code if a response was received
+  console.log('code:', response && response.statusCode); // Print the response status code if a response was received
 });
