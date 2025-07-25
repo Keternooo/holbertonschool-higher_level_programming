fa6e47635885645760082d754619cc76fbdc280d
root <keterno@proton.me>
2025-07-25 12:46:38 +0000

    Firth Day

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,5 +2,5 @@
 const request = require('request');
 request(process.argv[2], (e, s, b) => {
 	if(e) return;
-	console.log([...(b.matchAll('https://swapi-api.hbtn.io/api/people/18/'))].length);
+	console.log([...(b.matchAll('/people/18/'))].length);
 });
