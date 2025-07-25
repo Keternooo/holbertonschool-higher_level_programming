46a0581d1d2c19c5845884a283d46297902f0bff
root <keterno@proton.me>
2025-07-25 12:26:18 +0000

    Firth Day

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,5 +3,5 @@
 const request = require('request');
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], async (err, st, bo) => {
   if (err) return;
-  console.log((await JSON.parse(bo).title);
+  console.log((await JSON.parse(bo)).title);
 });
