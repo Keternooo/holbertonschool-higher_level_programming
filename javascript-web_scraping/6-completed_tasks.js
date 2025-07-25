555c30f2fa623dbc5a87828cfd1a8c3774f262e4
root <keterno@proton.me>
2025-07-25 12:56:31 +0000

    Firth Day

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,6 +4,6 @@ const request = require('request');
 request(process.argv[2], async (e, s, b) => {
   if (e) return;
   const obj = {};
-  (await JSON.parse(b)).forEach((a, i) => {if (a.completed) obj[i + 1] = a.userId; });
+  (await JSON.parse(b)).forEach((a, i) => {if (a.completed) obj[i + 1] = a.id; });
   console.log(obj);
 });
