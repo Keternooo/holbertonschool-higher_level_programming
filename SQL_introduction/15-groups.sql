c371beda9b2e2a87cf9d22bd129574309c567669
root <keterno@proton.me>
2025-07-25 08:44:03 +0000

    Firth Day

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- Count how manu got the score
 
-SELECT score, COUNT(score) AS 'number' FROM second_table GROUP BY score HAVING COUNT(score) > 0;
+SELECT score, COUNT(score) AS 'number' FROM second_table GROUP BY score HAVING COUNT(score) > 0 ORDER BY score ASC;
