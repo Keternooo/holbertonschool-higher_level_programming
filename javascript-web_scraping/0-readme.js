f7e9376dde10b5b9e0205e9dd95023de359c2ac1
root <keterno@proton.me>
2025-07-25 11:44:58 +0000

    Firth Day

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const { readFileSync } = require('fs');
 
-readFileSync(process.argv[2], 'utf-8').catch(a => console.log(a));
+readFileSync(process.argv[2], 'utf-8').then(a => console.log(a)).catch(a => console.log(a));
