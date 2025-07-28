41aa3440f1ad7b30ff3f93dd944e8eb7043e5db9
root <keterno@proton.me>
2025-07-28 10:10:59 +0000

    Sorry for testing

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,5 @@
 flag = 0
 
 for i in range(26, 0, -1):
-    print("{}".format((chr(i + 96).upper() if i % 2 != 0 else chr(i + 96))), end="")
+    a = i +96
+    print("{}".format((chr(a).upper() if i % 2 != 0 else chr(a))), end="")
