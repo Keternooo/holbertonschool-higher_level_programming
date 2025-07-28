8eae8112f931cc1eb503e6866e555e3f21ac1758
root <keterno@proton.me>
2025-07-28 07:29:52 +0000

    Testing

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
-import requests
-req = requests.get('https://monip.io')
-print(req.text)
+import random
+
+number = random.randint(-10000, 10000)
+lastDigit = abs(number) % 10
+
+if number < 0:
+    lastDigit = -lastDigit
+
+print("Last digit of", number, "is", lastDigit, end='')
+
+if lastDigit > 5:
+    print(" and is greater than 5")
+elif lastDigit == 0:
+    print(" and is 0")
+else:
+    print(" and is less than 6 and not 0")
