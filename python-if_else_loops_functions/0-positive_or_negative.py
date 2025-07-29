4717e407a2bb9409b2d801b047961b046b38cb6e
root <keterno@proton.me>
2025-07-29 09:33:29 +0000

    29/07

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-
-import requests
-res = requests.get('https://monip.io')
-print(res.text)
+import random
+number = random.randint(-10, 10)
+if number == 0:
+    print(str(number) + ' is zero')
+elif number < 0:
+    print(str(number) + ' is negative')
+elif number > 0:
+    print(str(number) + ' is positive')
